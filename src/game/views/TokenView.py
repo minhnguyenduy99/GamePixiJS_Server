b8e18a0bffaf9b2b15dca6ab5da1acd2561cef5e
minhnguuyenduy <minhnguyenduy99@gmail.com>
2020-07-23 06:24:13 +0700

    Add time to expire of token

--- src/game/views/TokenView.py
+++ src/game/views/TokenView.py
@@ -12,7 +12,7 @@ import datetime
 def generate_access_token(user):
   access_token_payload = {
     'user_id': user.id,
-    'exp': datetime.datetime.now() + datetime.timedelta(days=0, minutes=5),
+    'exp': datetime.datetime.now() + datetime.timedelta(days=0, minutes=20),
     'iat': datetime.datetime.now()
   }
   access_token = jwt.encode(access_token_payload, settings.SECRET_KEY, algorithm='HS256').decode('utf-8')
